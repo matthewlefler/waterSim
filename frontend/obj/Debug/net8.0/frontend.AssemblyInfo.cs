c1604262a7300986a47d8777e4cd1175d8595d1c
matthewlefler <matthewlefler1@gmail.com>
2025-03-20 18:19:59 -0400

    reset to make untracted files untracked 2

--- frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0ff9dca319c33ca3c1017ba6d371cb8bf49241")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac1e90ebcd1596a0c2e643c2961b89fbcabe551c")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
